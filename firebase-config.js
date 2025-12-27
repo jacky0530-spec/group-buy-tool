a04de9908a1118b4ad5cc27ed9cc8b44d8bced9f
jacky0530-spec <jacky0530@gmail.com>
2025-12-27 16:27:04 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDDrWplJvsFm5CzacbUaLH3d4RXmlVQNw",
+  authDomain: "group-buy-tool.firebaseapp.com",
+  projectId: "group-buy-tool",
+  storageBucket: "group-buy-tool.firebasestorage.app",
+  messagingSenderId: "215424664350",
+  appId: "1:215424664350:web:0f608b731de5938456bf0f",
+  measurementId: "G-7D1FL6CFGQ"
+};
